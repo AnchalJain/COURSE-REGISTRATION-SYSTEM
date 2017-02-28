fe8d43680581b6216679c01b76ed432730374445
AnchalJain <anchalj109@gmail.com>
2017-02-28 19:16:03 +0530

    add bitsat page

---
+++ BITSAT.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>R.G. Academy</title>
+	<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+      	<link rel="stylesheet" href="bootstrap-3.3.5-dist/css/bootstrap.min.css">
+        <link href="https://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet"/>
+   		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<?php require 'header.php'; ?>
+
+<div class="container">
+	<h3>BITSAT</h3>
+	<hr width="80%">
+	<br>
+
+	<h4>
+	<b>A Computer Based Online Test for Admission to Integrated First Degree
+		programmes of BITS, Pilani</b>
+	</h4>	
+	<p class="well" style="text-align: justify;">
+	 The Birla Institute of Technology and Science (BITS) Pilani is an all India Institution declared as Deemed to be University under Section 3 of the UGC Act. Admissions to all the Integrated First Degree programmes of BITS, Pilani, at Pilani campus, Goa campus, and Hyderabad Campus for the academic year 2014-15 will be made on the basis of a Computer based Online Test conducted by BITS, Pilani.
+	</p>
+	<br>
+
+	<h4><b>Eligibility:</b></h4>
+	<br>
+	<h4><b>(i) For admission to all the above programmes except B.Pharm.(Hons.): </b><br></h4>
+	<p class="well" style="text-align: justify;">
+	Candidates should have passed the 12th examination of 10+2 system from a recognized Central or State board or its equivalent with Physics, Chemistry, and Mathematics and adequate proficiency in English.
+	Admission to all the programmes is subject to the conditions given below.
+	The candidate should have obtained a minimum of aggregate 75% marks in Physics, Chemistry and Mathematics subjects (if he/she has taken Mathematics in BITSAT) or a minimum of aggregate 75% marks in Physics, Chemistry and Biology subjects (if he/she has taken Biology in BITSAT) subjects in 12th examination, with at least 60% marks in each of the Physics, Chemistry, and Mathematics / Biology subjects. 
+	</p>
+	<br>
+	
+	<h4><b>Only Students who are appearing for 12th examination or who have passed 12th Examination in the previous year are eligible to appear in the BITSAT test.</b></h4>
+	<br>
+	<p class="well" style="text-align: justify;">
+	 	If a candidate has taken more than one attempt in 12th class or its equivalent, only his latest performance is considered, provided this attempt has been for the full component of subjects/courses prescribed. Students who have passed 12th examination. Admissions will be made purely on merit. The merit position of the candidate for admission will be based on the score obtained by the candidate in the BITSAT-2014. However, their eligibility for admission is subject to fulfilling the requirement of minimum marks in 12th examination, as mentioned above.
+	</p>
+	<br>
+
+	<h4><b>Details of BITSAT</b></h4>
+	<br>
+	<p class="well" style="text-align: justify;">
+		sComputer Based Online test’ means the candidate sits in front of a computer and the questions are presented on the computer monitor and the candidate answers the questions on the computer through the use of keyboard or mouse. Each computer is connected to a server, which prepares the question set and delivers it to the candidate on the computer. This is unlike the traditional paper-pencil based test, which is generally offered on a single day to all candidates. BITSAT-2014 will be offered over a period of time and the candidate can choose the Center, the Day and Time of his/her convenience to take the test, as described in the later sections.
+	</p>
+
+	<h4><b>BITSAT Test Format:</b></h4>
+	<br>
+	<p class="well" style="text-align: justify;">
+		BITSAT is of total 3-hour duration (without break). The test consists of four parts:<br>
+
+		<b>All questions are of objective type (multiple choice questions); each question with choice of four answers, only one being correct choice. Each correct answer fetches 3 marks, while each incorrect answer has a penalty of 1 mark (-1mark)</b>
+		No marks are awarded for questions not attempted. While the candidate can skip a question, the computer will not allow the candidate to choose more than one option as correct answer. There will be 150 questions in all. The number of questions in each part is as follows:
+	</p>
+	<br>
+
+	<div class="table table-responsive well">
+		<table class="table">
+			<thead class="well">
+				<tr>	
+				<th></th>
+				<th >Subject</th>
+				<th >No Of Question</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				<tr>
+					<td>
+						 	Part I <br><br>
+						 	Part II <br><br>
+						 	Part III <br><br><br>
+						 	Part IV
+					</td>
+
+					<td>
+							Physics<br><br>
+							Chemistry<br><br>
+							(a) English Proficiency<br>
+							(b) logical Reasoning<br><br>
+							Mathematics
+					</td>
+
+					<td>
+							40<br><br>
+							40<br><br>
+							15<br>
+							10<br><br>
+							45<br><br>
+							<b>Total: 150 </b>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		
+	</div>
+	<br>
+
+	<p style="text-align: justify;">
+	<b>
+		 There is no time limit for individual parts of the test. The candidate can go back and change any of his/her answers among the 150 questions.
+		If a candidate answers all the 150 questions (without skipping any question), the candidate will have an option of attempting 12 (twelve) extra questions, if there is still time left. These extra questions will be from Physics, Chemistry, and Mathematics/Biology only; four questions from each part. Further, once the candidate has opted for extra questions, he cannot go back for correction of any of the earlier answered 150 questions. The questions are so designed that a good student will be able to answer 150 questions in 180 minutes. The extra questions (a maximum of 12) will give a chance to highly meritorious candidates to score higher. However, candidates should keep in mind the fact that there is negative marking for wrong answers and any attempt to answer the questions by pure guessing of the answers is not likely to have any advantage, but may result in a reduction in the total score.
+		For further details visit : BITSAT Official Website
+
+	</b>
+	</p>
+	
+</div>
+</body>
+</html>
