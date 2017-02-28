3285f2b5838badd4cdbb0c260677e15c8598fa8b
AnchalJain <anchalj109@gmail.com>
2017-02-28 19:14:49 +0530

    add About us page

---
+++ bs.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+   		<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+      <link rel="stylesheet" href="bootstrap-3.3.5-dist/css/bootstrap.min.css">
+      <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+      <link href="https://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet"/>
+   		<style>
+      .modal-header, h4, .close {
+      background-color: #282828;
+      color:white !important;
+      text-align: center;
+      font-size: 30px;
+      }
+      .modal-footer {
+      background-color: #282828;
+      }
+      </style>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		  <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+      <body>
+      
+      <?php require 'header.php'; ?>
+
+        
+      <br>
+
+      <div id="about">
+        <div class="container">
+          
+          
+          <h1>About the Academy</h1>
+            <hr width="800px">
+            <center>
+              <b>Welcome!</b>
+            </center>
+            <br></br>
+            <div class="para well">
+            <p> 
+              RG Academy is a firmly established institution which has become a symbol of quality 
+              learning for those aspirants appearing in the entrance examinations of prestigious IITs, NITs and 
+              other engineering colleges. 
+            </p>
+            <b>The Academy is working under the mentorship of Mr. Girish Jain and Mr. Ratnesh Dayal. </b>
+            <br>
+            <p>
+              Both the directors (Mr. Girish Jain and Mr. Ratnesh Dayal) strongly feel about the need for quality education and the requirement of proper guidance for young aspirants wanting to become successful technocrats. As they had themselves graduated from IIT Delhi, they were fully conversant with the requirements of the prestigious IITs. Considering education as a noble profession, they started the academy with the specific aim of tutoring serious students for the IITs.
+            </p>
+        
+              The main aspect that makes RG Academy stand out is that compromise in quality is not tolerated. The faculty consists of experienced and dedicated teachers who themselves have graduated from the IITs, NITs etc. Their dedication and stability provides right ambience to the students, who are motivated to put in extra effort which makes all the difference.
+            <br> </br>
+    
+            <center>
+            <b>Message From The Academy</b>
+            </center>
+              <br>
+            <p>
+              After considering various possibilities for the future, if you are seriously aspiring to be an engineer, of repute, by profession, then you need to be determined enough to gain admission with a good rank, in an institution worth its mettle.
+              To get into a good institution, you must remember that the competition in immense and growing. To achieve success in the entrance exams (with a good rank) will require specialized skills and personalized training.
+            </p>
+            <br>
+      
+              We, at RG Academy, help you to acquire the specialized skills.<br>
+            <p>
+              The approach is two pronged. The first is that constant motivation is imparted. We help you to develop your confidence in yourself, strengthen your determination to achieve your goal, make hard work look easy, and above all, help you to maintain your focus on the goal.
+            </p>
+            <p>
+              We ask you to place your trust in us and our methodology. We will take you by the hand, teach you to set small goals, achieve them and then follow them with bigger ones. The going will be tough. Each goal will require extra efforts. But over time, these extra efforts become a habit. We will teach you to conduct your studies in a systematic and scientific manner, for haphazard studies do not serve any purpose. We will encourage you to interact with your teaches and clear your doubts / problems on a daily basis.
+            </p>
+      
+            <p>
+              We are also aware that as a student you will be concerned about the Board studies as well as about the All India Competitive Exams. It has always been our intrinsic belief that the school studies for the Board Exams are as important as the entrance exams. As the syllabai for both exams are the same, but the treatment is different, we take extra efforts to impart knowledge in both the streams. Thus, the Board treatment and the entrance exam treatment go hand in hand.
+              Remember, you have two support systems actively working for you. One-your family. You don’t have to worry about your fees, food, daily chores etc. Second – the academics. This we take care of.
+            </p>
+      
+            <p>
+              However, in the ultimate analysis, both the systems can show you the way, motivate you, solve your problems – but, the own individual motivation and focus has to be from inside you. It should be like a burning desire to achieve what you have focused on, pushing you to do better and better till you do not need any external motivation. Learn to love what you are learning, become totally and completely immersed in your subjects. 
+            </p>
+            <p> Pure dedication is what is required. Trust us, and we will guide you on the right path.</p> 
+            <br>
+
+            <b>GOOD WISHES! </b>
+            <hr>
+            <br>
+          </div>
+        </div>
+      </div>
+
+          
+          
+
+
+
+  </body>
+  
+</html>
