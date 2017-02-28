0f033b7595db3fc75781ad8efcf5c073bfabe06c
AnchalJain <anchalj109@gmail.com>
2017-02-28 19:18:40 +0530

    add jee mains page

---
+++ jee_m_a.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>R.G. Academy</title>
+		<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+      	<link rel="stylesheet" href="bootstrap-3.3.5-dist/css/bootstrap.min.css">
+      	<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+      	<link href="https://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet"/>
+   		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		 <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+<?php require 'header.php'; ?>
+<div class="container">
+	<h3>JEE MAINS + ADVANCED</h3>
+	<hr width="80%">
+
+	<h4>Joint Entrance Examination</h4>
+	<br>
+	<p class="well" style="text-align: justify;">
+		JEE is the common entrance test for admissions into central, state and private engineering institutions i.e. IITs, NITs, DITs and IISERs. 
+	</p>
+
+	<h4>JEE MAIN</h4>
+	<br>
+	<p class="well" style="text-align: justify;">
+
+		The admission to Undergraduate Engineering Programsat NITs, IIITs, other centrally funded Technical Institutions, Institutions under several participating State Governments, and several other Institutions shall include the performance in the JEE (Main). For admission to NITs, Centrally Funded Technical Institutions (CFTIs) like IIITs etc. and other participating Institutions, the merit/rank list shall be prepared based on 40% weightage to school Boards marks in class 12thor equivalent examination and 60% weightage to JEE(Main). The weightage to school Board/Equivalent examination marks shall be considered only after normalization. The JEE (Main) will also be the eligibility test for the JEE (Advanced), which the candidate has to take if he/she is aspiring for admission to the undergraduate programmes offered by the IITs/ ISM Dhanbad.
+
+	</p>
+
+	<h4> Details Of Examination</h4>
+	<br>
+	<p class="well" style="text-align: justify;">
+		JEE (Main) – has two papers, Paper-1 (B.E./B. Tech.) and Paper-2 (B. Arch./B. Planning.). Depending upon the course(s) candidates are seeking admission, they may take Paper-1 (B. E./B. Tech.), or Paper 2(B.Arch./B.Planning.), or both.
+
+	</p>
+	</div>
+	<br>
+	<div class="container">
+	<div class="table table-responsive table-bordered">
+			<table class="table">
+				<thead style="background-color:lightgrey">
+					<tr>
+					<th style="text-align: center;">PAPER</th>
+					<th style="text-align: center;">SUBJECTS</th>
+					<th style="text-align: center;">TYPE OF QUESTIONS</th>
+					<th style="text-align: center;">MODE OF EXAM</th>
+					</tr>
+				</thead>
+				
+				<tbody>
+					<tr>
+						<td style="text-align: center;">
+							Paper 1<br>
+							(B.E./B.Tech)
+						</td>
+						<td style="text-align: center;">
+							Physics,Chemistry &<br>
+							Maths
+						</td>
+						<td style="text-align: center;">
+							Objective type with equal weightage<br>
+							to Physics, Chemistry & Mathematics 
+						</td>
+						<td style="text-align: center;">
+							Pen and Paper Based Exam<br>
+									OR<br>
+							Computer Based Exam<br> 
+						</td>
+					</tr>
+
+					<tr>
+						<td style="text-align: center;">
+								Paper 2 <br>
+							(B.Arch/B.Planning)	
+						</th>
+						<td style="text-align: center;">
+							Mathematics – Part I<br>
+							Aptitude Test – Part II<br>
+										&<br>
+							Drawing Test – Part III<br>
+						</td>
+						<td style="text-align: center;">
+							Objective Types
+						</td>
+						<td style="text-align: center;">
+							Only Pen and Paper<br>
+								Based Exam 
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>			
+	</div>
+
+
+	<div class="container">
+		<h4> JEE ADVANCED</h4>
+			<p class="well" style="text-align: justify;">
+			Only top 150,000 candidates (including all categories) from the JEE "MAIN" will be qualified to appear in the JEE "ADVANCED" examination.
+			</p>
+			<br>
+		<h4>JEE Selcetion Process</h4>	
+			<p class="well" style="text-align: justify;">
+				Admissions to IITs will be based only on category wise All India Rank (AIR) in JEE ADVANCED subject to the condition that candidate must be among the top 20 percentile in his/her board category wise or should have secured a minimum of 70% marks in his/her respective board examination.
+			</p>
+			<br>
+		<h4>JEE ADVANCED Paper Pattern</h4>
+			<p class="well" style="text-align: justify;">
+				The Joint Entrance Examination (Advanced) 2015 [JEE (Advanced) 2015] will be conducted by the seven zonal Indian Institutes of Technology (IITs) under the guidance of the Joint Admission Board (JAB) 2015. The performance of a candidate in this examination will form the basis for admission to the Bachelor's, Integrated Master's and Dual Degree programs (entry at the 10+2 level) in the sixteen IITs and the Indian School of Mines (ISM). The examination consists of two papers, Paper 1 and Paper 2, each of 3 hour duration and will be held as per the following schedule: 
+			</p>
+			<br>	
+
+		<div class="table table-responsive well" style="border: 0px;">
+			<table class="table">
+				<tbody>
+					<tr>
+						<td>	
+							Day,Date<br><br><br>
+							Time
+						</td>
+						<td>Sunday,May 24,2016				
+							<br><br><br>
+							Paper-I 9:00 to 12:00 IST<br>
+							<br>
+							Paper-II 14:00 to 17:00 IST<br>
+						</td>
+					</tr>
+				</tbody>
+				
+			</table>	
+		</div>
+		<br>
+	</div>
+
+</body>
+</html>
