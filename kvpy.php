feb69428c915570b73c82f301225a6f98969d6d8
AnchalJain <anchalj109@gmail.com>
2017-02-28 19:16:53 +0530

    kvpy file

---
+++ kvpy.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>R.G. Academy</title>
+	<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+      	<link rel="stylesheet" href="bootstrap-3.3.5-dist/css/bootstrap.min.css">
+        <link href="https://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet"/>
+   		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	
+	<?php require 'header.php'; ?>
+
+	<div class="container">
+		<h3>KYPY</h3>
+		<hr width="80%">
+		<h4><b> Kishore Vaigyanik Protsahan Yojana (KVPY) </b></h4>
+		<p class="well" style="text-align: justify;">
+			The Kishore Vaigyanik Protsahan Yojana (KVPY) is an on-going National Program of Fellowships in Basic Sciences to attract exceptionally and highly motivated students for pursuing basic science courses and research career in science. The objectives of the program are to identify students with talent and aptitude for research; help them realize their potential in their studies; encourage them to take up research careers in Science, and ensure the growth of the best scientific minds for research and development in the country.<br>
+
+
+			The selection of students is made from those studying in XI standard to 1st year in any undergraduate Program in Basic Sciences namely B.Sc./B.S./B.Stat./B.Math./Int. M.Sc./M.S. in Mathematics, Physics, Chemistry and Biology having aptitude for scientific research. Special groups / committees are set up at IISc, to screen the applications and conduct an aptitude test at various centres in the country. Generous scholarships are provided up to the pre-Ph.D. level to the selected KVPY Fellows. 
+		</p>
+		<br>
+		<h3>Selection Process</h3><br>
+		<h4> <b>1. Aptitue Test </b></h4>
+		<p class="well" style="text-align: justify;">
+			After scrutiny of application forms and meeting the eligibility criteria for various streams, all eligible students will be called for a written aptitude test conducted both in Hindi and English at different centers across the country in October . The details of the venue of the aptitude test for the eligible students and their seat number will be published in the KVPY website during the first week of October.
+		</p><br>
+
+		<h4><b>2. Interview</b></h4>
+		<p class="well" style="text-align: justify;">
+			Based on the performance in the aptitude test, short-listed students will be invited for an interview which is the final stage of the selection procedure. 	
+		</p>
+
+		<h3>Eligibilty</h3><br>
+		<h4><b>Stream SA :</b></h4>
+		<p class="well" style="text-align: justify;">
+			Students enrolled in the XI Standard (Science Subjects) during the academic year 2014-15 and have secured a minimum of 80% (70% for SC/ST) marks in aggregate in MATHEMATICS and SCIENCE subjects in the X Standard Board Examination. Students promoted through both the Summative Assessments-II conducted by the school and conducted by the Board are eligible to apply, if they satisfy the eligibility criteria. If the students have obtained a letter grade in the qualified examination, they are requested to convert it into an appropriate % and fill-up the application form. 			
+		</p>
+		<br>
+
+		<h4><b>Stream SA - Mentoring Scheme:</b></h4>
+		<p class="well" style="text-align: justify;">
+			Eligibility same as above and the students need not apply separately for the Mentoring Scheme. A Mentoring list over and above the regular merit list will be prepared. The students under the mentorship list will not be eligible for any fellowship. Their fellowship will be activated when these students enrol in an undergraduate course in Basic Sciences (B.Sc./B.S./Int.M.Sc./M.S.) after their XII Standard/(+2). During the mentoring period of one year, they will be invited for Summer Programme/ Vijyoshi Program and their travel and other related expenses will be met by KVPY. 
+		</p>
+		<br>
+
+		<h4><b>Stream SX:</b></h4>
+		<p class="well" style="text-align: justify;">
+			Students enrolled in XII Standard/ (+2) (Science subjects) during the academic year 2014-15 and aspiring to join an undergraduate program in Basic Sciences (B.Sc./B.S./Int. M.Sc./M.S.) for the session 2013–14 provided they have secured a minimum of 80% (70% for SC/ST) marks in aggregate in MATHEMATICS and SCIENCE subjects in the X Standard Board Examination.
+		</p>
+		<br>
+
+		<h4><b>Stream SB :</b></h4>
+		<p class="well" style="text-align: justify;">
+		Students enrolled in the 1st year of B. Sc./B.S./Int. M.Sc./M.S. during the academic year 2014-15 and have secured a minimum of 60% (50% for SC/ST) marks in aggregate in SCIENCE subjects in the XII Standard Board Examination.
+		</p>
+		<br>
+	</div>	
+</body>
+</html>
