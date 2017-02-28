85489e649f8de96a75c67ddc301d85e7455b9b53
AnchalJain <anchalj109@gmail.com>
2017-02-28 19:15:08 +0530

    add faculty details

---
+++ faculty.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>R.G. Academy</title>
+  <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <link rel="stylesheet" href="bootstrap-3.3.5-dist/css/bootstrap.min.css">
+        <link href="https://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet"/>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<?php require 'header.php'; ?>
+
+
+ <div class="wrapper container">
+          <h1> Faculty Team </h1>
+          <hr width="80%">
+          <center>
+          Presenting the Best Result producing<br>
+                Faculty Team under the<br>
+                  Mentorship of<br> 
+          </center>
+          <br>
+          <hr width="80%">
+          </div>
+
+            <div class="container">
+            <div class="row">
+              
+              <div class="col-md-2"></div>
+              <div class="col-md-4 "> 
+               
+                <img class="img-responsive" src="assets/Ratnesh dayal.jpg" alt="Ratnesh Dayal">
+                   <p style="text-align:justify;">
+                    A perfectionist at his work, R.D.Sir is probably the only one who has in-depth knowledge in both maths and chemistry. His methodology in teaching combined with vast knowledge makes him one of the best mentors for IIT-JEE preparation. He not only prepares students for the toughest competition in the exam, but also imbibes analytical skills which are helpful in future endeavours too.
+                  </p>
+                  </div>                 
+                
+               <!-- <div class="col-md-4 col-md-offset-8">
+                <p>
+                <b>Mr.Ratnesh Dayal</b>
+                  b.tech,IIT-Delhi
+                   (Director)
+                </p>
+              </div>                 -->
+
+              <div class="col-md-2"></div>
+
+              <div class="col-md-4">
+                <div id="director2">
+                  <img class="img-responsive" src="assets/Girish Jain.jpg" alt="Girish Jain">
+                    <p style="text-align: justify;" class="pull-left">
+                      A pioneer of IIT-JEE preparation in Ajmer, he is a thorough professional whose impeccable class delivery clubbed with his vast experience makes him one of the best Physics teachers. An IITian himself, he has mentored many young minds to the path of success since his college days.
+                    </p>
+                  
+                </div>
+                
+              </div>
+            </div>
+            </div>
+
+
+                <div class="container">
+                <h2 style="color: red">Physics</h2>
+                  <br>
+                  <div class="row">
+                      <div class="col-md-3" style="margin: 1px;"></div>
+                      <div class="col-md-2 col-xs-4 col-sm-4" style="background-color: aliceblue; margin: 10px; padding: 5px;">
+                        <img class="img-responsive center-block" src="assets/GirishJain2.JPG" alt="Girish Jain"  style="padding: 5px;">
+                          <p class="text-center">
+                            <b>Girish Jain</b> <br>
+                            &nbsp;b.Tech,IIT-Delhi<br></br>
+                            &nbsp;Exp 14 years
+                          </p>
+                      </div>
+
+                                   
+                      <div class="col-md-2 col-xs-4 col-sm-4" style="background-color: lavender; margin:10px; padding: 10px;">
+                        <img class="img-responsive center-block" src="assets/prateek.JPG" alt="Prateek Jain" >
+                          <p class="text-center">
+                            <b>Prateek Jain</b> <br>
+                             &nbsp;b.Tech-MNIT,Jaipur<br></br>
+                            &nbsp;Exp 7 years
+                          </p>
+                      </div>
+
+
+                      <div class="col-md-2 col-xs-4 col-sm-4" style="background-color: aliceblue; margin: 10px;  padding: 5px;">
+                        <img class="img-responsive center-block" src="assets/sidharthjain.JPG" alt="Siddharth Jain" style="padding: 5px;">
+                          <p class="text-center">
+                            <b>Sidharth Jain</b> <br>
+                            &nbsp;b.Tech-MNIT,Jaipur<br></br>
+                            &nbsp;Exp 14 years
+                          </p>
+                      </div> 
+                  </div>     
+                     
+
+
+                  <div class="row">
+                    <div class="col-md-3" style=" margin:1px"></div>
+                    <div class="col-md-2 col-xs-4 col-sm-4" style="background-color: lavender; margin: 10px; padding: 5px;">
+                      <img class="img-responsive center-block" src="assets/Iqlab.JPG" alt="Iqlab"  style="padding: 5px;">
+                        <p class="text-center">
+                          <b>Iqlab Singh Sahani</b> <br>
+                           &nbsp;b.Tech-MBM,Jodhpur<br></br>
+                        </p>
+                    </div>
+                                   
+                    <div class="col-md-2 col-xs-4 col-sm-4" style="background-color: aliceblue; margin:10px; padding: 10px;">
+                      <img class="img-responsive center-block" src="assets/Ankit.JPG" alt="Ankur Maheshwari" >
+                        <p class="text-center">
+                          <b>Ankur Maheshwari</b> <br>
+                            &nbsp;b.Tech-GEC,Ajmer<br></br>
+                        </p>
+                    </div>
+                  </div>
+                    
+                
+                  <h2 style="color: red">Chemistry</h2>
+                    <br>
+                    <div class="row">
+                      <div class="col-md-3" style="margin: 1px;"></div>
+                      <div class="col-md-2 col-xs-4 col-sm-4" style="background-color: aliceblue; margin: 10px; padding: 5px;">
+                        <img class="img-responsive center-block" src="assets/RD.JPG" alt="Ratnesh Dayal"  style="padding: 5px;">
+                          <p class="text-center">
+                            <b>Ratnesh Dayal</b> <br>
+                            &nbsp;b.Tech,IIT-Delhi<br></br>
+                            &nbsp;Exp 14 years
+                          </p>
+                      </div>
+
+                      <div class="col-md-2 col-xs-4 col-sm-4" style="background-color: lavender; margin:10px; padding: 10px;">
+                        <img class="img-responsive center-block" src="assets/gauravC.JPG" alt="Gaurav Mishra" >
+                          <p class="text-center">
+                            <b>Gaurav Mishra</b> <br>
+                             &nbsp;b.Tech-IIT Kanpur<br></br>
+                            &nbsp;Exp 4 years
+                          </p>
+                      </div>
+
+                      <div class="col-md-2 col-xs-4 col-sm-4" style="background-color: aliceblue; margin: 10px;  padding: 5px;">
+                        <img class="img-responsive center-block" src="assets/ navneetC.JPG" alt="Navneet Jain" style="padding: 5px;">
+                          <p class="text-center">
+                            <b>Navneet Jain</b> <br>
+                            &nbsp;M.Sc Chem,NET,SLET<br></br>
+                            &nbsp;Exp 5 years
+                          </p>
+                      </div> 
+                    </div>
+
+                    <div class="row">
+                      <div class="col-md-3" style=" margin:1px"></div>
+                      <div class="col-md-2 col-xs-4 col-sm-4" style="background-color: lavender; margin: 10px; padding: 5px;">
+                        <img class="img-responsive center-block" src="assets/bachatwatsor.JPG" alt="J.M. Bacchhawat"  style="padding: 5px;">
+                          <p class="text-center">
+                            <b>J.M. Bacchhawat</b> <br>
+                            &nbsp;PH.D<br></br>
+                            &nbsp;Exp. 35 years
+                          </p>
+                      </div>
+
+                      <div class="col-md-2 col-xs-4 col-sm-4" style="background-color: aliceblue; margin:10px; padding: 10px;">
+                      <img class="img-responsive center-block" src="assets/Chem.JPG" alt="Anirudh Shridharan" >
+                        <p class="text-center">
+                          <b>Anirudh Shridharan</b> <br>
+                            &nbsp;b.Tech SASTRA University<br></br>
+                            &nbsp; Exp 2 years
+                        </p>
+                      </div>
+                    </div> 
+
+
+                    <h2 style="color:red"> Maths </h2>
+                    <div class="row">
+                    <div class="col-md-3" style="margin: 1px;"></div>
+                      <div class="col-md-2 col-xs-4 col-sm-4" style="background-color: aliceblue; margin: 10px; padding: 5px;">
+                        <img class="img-responsive center-block" src="assets/RD.JPG" alt="Ratnesh Dayal"  style="padding: 5px;">
+                          <p class="text-center">
+                            <b>Ratnesh Dayal</b> <br>
+                            &nbsp;b.Tech,IIT-Delhi<br></br>
+                            &nbsp;Exp 14 years
+                          </p>
+                      </div>
+
+                      <div class="col-md-2 col-xs-4 col-sm-4" style="background-color: lavender; margin:10px; padding: 10px;">
+                        <img class="img-responsive center-block" src="assets/kamal.JPG" alt="Kamal Kishore Verma" >
+                          <p class="text-center">
+                            <b>Kamal Kishore Verma</b> <br>
+                             &nbsp;B.Sc.(hons.),M.Sc<br></br>
+                            &nbsp;Exp 8 years
+                          </p>
+                      </div>
+
+                       <div class="col-md-2 col-xs-4 col-sm-4" style="background-color: aliceblue; margin:10px; padding: 10px;">
+                      <img class="img-responsive center-block" src="assets/Ashutosh.JPG" alt="Ashutosh Dayal" >
+                        <p class="text-center">
+                          <b>Ashutosh Dayal</b> <br>
+                            &nbsp;B.Sc.(hons.),M.Sc<br></br>
+                            &nbsp; Exp 1 year
+                        </p>
+                      </div>
+                    </div>  
+
+                    <div class="row">
+                    <div class="col-md-3" style="margin: 1px;"></div>  
+                      <div class="col-md-2 col-xs-4 col-sm-4" style="background-color: lavender; margin:10px; padding: 10px;">
+                      <img class="img-responsive center-block" src="assets/anurag.JPG" alt="Anurag Singh Chauhan">
+                        <p class="text-center">
+                          <b>Anurag Singh Chauhan</b> <br>
+                            &nbsp;b.Tech IIT Dhanbad<br></br>
+                        </p>
+                      </div>
+
+                      <div class="col-md-2 col-xs-4 col-sm-4" style="background-color: aliceblue; margin:10px; padding: 10px;">
+                      <img class="img-responsive center-block" src="assets/7.JPG" alt="Sandeep Sharma">
+                        <p class="text-center">
+                          <b>Sandeep Sharma</b> <br>
+                            &nbsp;Mental Ability<br></br>
+                        </p>
+                      </div>
+
+                      <div class="col-md-2 col-xs-4 col-sm-4" style="background-color: lavender; margin:10px; padding: 10px;">
+                        <img class="img-responsive center-block" src="assets/maths.JPG" alt="Apurva Khemani" >
+                          <p class="text-center">
+                            <b>Apurva Khemani</b> <br>
+                             &nbsp;M.Sc<br></br>
+                          </p>
+                      </div>
+                      
+
+
+                </div> 
+
+
+              
+  </body>
+  
+</html>
