57b5b9d4daedece8b3661363379071d95d45c437
AnchalJain <anchalj109@gmail.com>
2017-02-28 19:17:07 +0530

    fee structure detaisl

---
+++ fee_structure.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RG Academy</title>
+	<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<link rel="shortcut icon" href="rg-academy-logo-90x90.ico" />
+    	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+      	<link rel="stylesheet" href="bootstrap-3.3.5-dist/css/bootstrap.min.css">
+      	<link rel="stylesheet" type="text/css" href="css/X_students.css">
+      	<link href="https://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet"/>
+   		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+	<?php require 'header.php'; ?>
+	<div class="container">
+		<h3>Fee Structure</h3>
+		<hr width="80%">	
+	</div>
+
+	<div class="container">
+			<div class="row">
+				<div class="col-md-2"></div>
+				<div class="col-md-4">
+					<h4><b><i>English Medium</i></b></h4>	
+				</div>
+			</div>
+	</div>
+
+	<div class="container">
+	<div class="table table-responsive table-bordered">
+			<table class="table">
+			<thead style="background-color: lightgrey">
+				<tr>
+					<th>Courses</th>
+					<th>Total Fee</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				<tr>
+					<td> 
+						 JEE MAIN + ADVANCED<br>
+						(X to XI Moving Students)
+					</td>
+
+					<td>
+						90,000/-
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						  JEE MAIN + ADVANCED<br>
+						(XI to XII Moving Students)
+					</td>
+
+					<td>
+						92,000
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						   JEE MAINS + ADVANCED<br>
+						(XII Appeared/Passed Students)
+					</td>
+
+					<td>
+						96,000/-
+					</td>
+				</tr>
+			</tbody>
+			</table>
+	<br>		
+	</div>
+
+	<div class="container">
+			<div class="row">
+				<div class="col-md-2"></div>
+				<div class="col-md-4">
+					<h4><b><i>Hindi Medium</i></b></h4>	
+				</div>
+			</div>
+	</div>
+
+	<div class="container">
+			<div class="table table-responsive table-bordered">
+			<table class="table">
+			<thead style="background-color: lightgrey">
+				<tr>
+					<th>Courses</th>
+					<th>Total Fee</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				
+			</tbody>
+			<tr>
+					<td> 
+						 JEE MAIN + ADVANCED<br>
+						(X to XI Moving Students)
+					</td>
+
+					<td>
+						62,000/-
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						  JEE MAIN + ADVANCED<br>
+						(XI to XII Moving Students)
+					</td>
+
+					<td>
+						62,000
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						   JEE MAINS + ADVANCED<br>
+						(XII Appeared/Passed Students)
+					</td>
+
+					<td>
+						96,000/-
+					</td>
+				</tr>
+			</tbody>
+			</table>
+			</div>
+	<br>		
+	</div>
+
+	<div class="container">
+			<div class="row">
+				<div class="col-md-2"></div>
+				<div class="col-md-4">
+					<h4><b><i>SCHOLARSHIPS</i></b></h4>	
+				</div>
+			</div>
+	</div>
+
+
+	<div class="container">
+		<div class="table table-responsive table-bordered">
+		<table class="table">
+			<thead style="background-color: lightgrey">
+				<tr>
+						<td>
+							XI + JEE MAIN + ADVANCED<br>
+								(English Medium)
+						</td>
+
+						<td>
+							XI + JEE MAIN + ADVANCED<br>
+								(Hindi Medium)
+						</td>
+				</tr>
+			</thead>
+			
+			<tbody>
+				<tr>
+					<td style="text-align: justify;">
+						Scholarship upto 100% based on <br>
+						performance in Entrance Test
+					</td>
+
+					<td style="text-align: justify;">
+						State Merit : 50%<br>
+						District Merit : 40%<br>
+						More than 90% in class X : 25%<br>
+						More than 85% in class X : 15%<br>
+						More than 80% in class X : 10% <br>
+					</td>
+
+				</tr>
+			</tbody>	
+		</table>
+		</div>
+		<br><br>		
+
+		<div class="table table-responsive table-bordered">
+		<table class="table">
+			<thead style="background-color: lightgrey">
+				<tr>
+						<td>
+							XI + JEE MAIN + ADVANCED English/Hindi Medium
+
+						</td>
+						<td></td>
+				</tr>
+			</thead>
+
+			<tbody>
+				<tr>
+					<td>Qualified in JEE Advanced(AIR upto 10,000)</td>
+					<td>30%</td>
+				</tr>
+
+				<tr>
+					<td>Qualified in JEE Advanced(AIR above 10000)</td>
+					<td>20%</td>
+				</tr>
+
+				<tr>
+					<td>Appeared in JEE Advanced</td>
+					<td>10%</td>
+				</tr>
+
+				<tr>
+					<td>More than 95% in CBSE</td>
+					<td>10%</td>
+				</tr>
+
+				<tr>
+					<td>More than 90% in RBSE</td>
+					<td>10%</td>
+				</tr>
+			</tbody>			
+		</table>
+		</div>	
+	</div>	
+
+</body>
+</html>
